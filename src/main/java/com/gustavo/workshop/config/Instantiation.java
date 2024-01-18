2231330610ee2d2a17c8331a769a729048def053
Gustavo Teles <gustavo.teles711@gmail.com>
2024-01-17 21:15:15 -0300

    operação de instanciar a base de dados

---
+++ src/main/java/com/gustavo/workshop/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.gustavo.workshop.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gustavo.workshop.Repository.UserRepository;
+import com.gustavo.workshop.domain.User;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
