7275c2ccb6ab741577651e631a59c58973ba6388
Gustavo Teles <gustavo.teles711@gmail.com>
2024-01-17 21:50:38 -0300

    usando adrão dto para retornar usuarios

---
+++ src/main/java/com/gustavo/workshop/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.gustavo.workshop.dto;
+
+import java.io.Serializable;
+
+import com.gustavo.workshop.domain.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 2716211130563741960L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+	}
+	
+	public UserDTO(User obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
--- src/main/java/com/gustavo/workshop/resources/UserResource.java
+++ src/main/java/com/gustavo/workshop/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.gustavo.workshop.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavo.workshop.domain.User;
+import com.gustavo.workshop.dto.UserDTO;
 import com.gustavo.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
