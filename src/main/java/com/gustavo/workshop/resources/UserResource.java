9c22c28431351c397651a6994ee61f4ed626ec13
Gustavo Teles <gustavo.teles711@gmail.com>
2024-01-18 14:38:05 -0300

    Atualização de usuario com put

--- src/main/java/com/gustavo/workshop/resources/UserResource.java
+++ src/main/java/com/gustavo/workshop/resources/UserResource.java
@@ -51,4 +51,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO, @PathVariable String id) {
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/gustavo/workshop/services/UserService.java
+++ src/main/java/com/gustavo/workshop/services/UserService.java
@@ -1,8 +1,10 @@
 package com.gustavo.workshop.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import org.bson.BsonUndefined;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +27,7 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
@@ -33,10 +35,25 @@ public class UserService {
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
-	
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		try {
+			Optional<User> newUser = repo.findById(obj.getId());
+			User user = newUser.get();
+			updateData(user, obj);
+			return repo.save(user);
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Object not found.");
+		}
+	}
+
+	private void updateData(User user, User obj) {
+		user.setName(obj.getName());
+		user.setEmail(obj.getEmail());
 	}
 }
